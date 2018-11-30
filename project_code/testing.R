d3e36fef1a2ff982a5e81506ba4f39eab741ce41
Alex Miller <alex.k.miller@gmail.com>
2018-11-30 09:26:14 -0500

    First commit

---
+++ project_code/testing.R
@@ -0,0 +1,14 @@
+list.of.packages <- c("data.table","emdi")
+new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(list.of.packages, require, character.only=T)
+
+
+#Loading PovcalNet data
+if(.Platform$OS.type == "unix"){
+  prefix = "~"
+}else{
+  prefix = "E:"
+}
+wd = paste0(prefix,"/git/emdi")
+setwd(wd)
